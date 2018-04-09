bad310a6452cbcbc4c96c3e01cdcfe5f611eaf55
brocktubre <brocktubre@gmail.com>
2018-04-08 21:00:39 -0500

    Added AWS certifications to footer. Cleaned up the image folder.

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -22,17 +22,19 @@ export class NavbarComponent implements OnInit {
     const url = this.router.url;
     switch (url) {
       case '/home':
-        this.isHomeActive = true;
-        this.titleService.setTitle('Home | Brock Tubre');
-        break;
+          this.isHomeActive = true;
+          this.titleService.setTitle('Home | Brock Tubre');
+          break;
         case '/portfolio':
-        this.isPortfolioActive = true;
-        this.titleService.setTitle('Portfolio | Brock Tubre');
-        break;
+          this.isPortfolioActive = true;
+          this.titleService.setTitle('Portfolio | Brock Tubre');
+          break;
         case '/about':
-        this.isAboutActive = true;
-        this.titleService.setTitle('About | Brock Tubre');
-        break;
+          this.isAboutActive = true;
+          this.titleService.setTitle('About | Brock Tubre');
+          break;
+        default:
+          this.titleService.setTitle('Home | Brock Tubre');
     }
   }
 
