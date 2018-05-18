c4969e1bf4adef52631841545fcf311836d7f25f
brocktubre <brocktubre@gmail.com>
2018-05-18 18:16:45 -0500

    Cleaned up some code.

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideosComponent implements OnInit {
   public videoSrc: string;
+  public year: string;
   constructor() { }
 
   ngOnInit() {
+    this.year = new Date().getFullYear().toString();
     this.videoSrc = 'https://s3.us-east-2.amazonaws.com/brocktubre-public-bucket/acloudguru_video.mov';
   }
 
