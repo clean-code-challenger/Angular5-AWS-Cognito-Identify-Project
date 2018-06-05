483048c927c1a94d49ec12bb4cbed2ab65043c65
Brock Tubre <brock.tubre@csra.com>
2018-06-05 13:16:30 -0500

    Removed keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  region: 'us-east-1',
-  public_bucket_name: 'brocktubre-s3-sandbox-bucket',
-  dynamodb_table_name: 'brocktubre-s3-lambda-dynamodb-integration-sandbox-table',
-  firebase: {
-    apiKey: 'AIzaSyDk4YyTdWhfQ1S-m9jw6vAefa6IYPRIb2U',
-    authDomain: 'brocktubre-bt-website.firebaseapp.com',
-    databaseURL: 'https://brocktubre-bt-website.firebaseio.com',
-    projectId: 'brocktubre-bt-website',
-    storageBucket: 'brocktubre-bt-website.appspot.com',
-    messagingSenderId: '675386683012'
-  }
+
 };
