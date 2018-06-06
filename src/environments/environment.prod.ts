9fb9bb0cea48bcf09f29f74a691eab117c5e7d80
Brock Tubre <brock.tubre@csra.com>
2018-06-06 12:53:39 -0500

    Oops

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,3 @@
 export const environment = {
-  production: true,
-  aws_identity_pool_id: 'us-east-1:456232b3-b6f5-4a11-957c-484660fe97c3',
-  region: 'us-east-1',
-  public_bucket_name: 'brocktubre-s3-sandbox-bucket',
-  dynamodb_table_name: 'brocktubre-s3-lambda-dynamodb-integration-sandbox-table',
-  firebase: {
-    apiKey: 'AIzaSyDk4YyTdWhfQ1S-m9jw6vAefa6IYPRIb2U',
-    authDomain: 'brocktubre-bt-website.firebaseapp.com',
-    databaseURL: 'https://brocktubre-bt-website.firebaseio.com',
-    projectId: 'brocktubre-bt-website',
-    storageBucket: 'brocktubre-bt-website.appspot.com',
-    messagingSenderId: '675386683012'
-  }
+
 };
