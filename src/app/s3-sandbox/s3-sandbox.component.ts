d9c338e70e40cdd14e94a933f775520fa09b6aab
Brock Tubre <brock.tubre@csra.com>
2018-06-08 14:57:14 -0500

    Some clean up code.

--- src/app/s3-sandbox/s3-sandbox.component.ts
+++ src/app/s3-sandbox/s3-sandbox.component.ts
@@ -71,7 +71,7 @@ export class S3SandboxComponent implements OnInit {
       this.s3SandboxService.uploadObjectToS3(this.bucketName, this.fileToUpload).subscribe(item => {
         this.myFileInputVal.nativeElement.value = null;
         this.fileToUpload = null;
-        setTimeout(this.loadObjects.bind(this), 2000);
+        setTimeout(this.loadObjects.bind(this), 5000);
       });
     }
   }
@@ -147,6 +147,22 @@ export class S3SandboxComponent implements OnInit {
       return false;
     }
     return true;
+  }
+
+  public formatBytes(bytes: number): string {
+    if (bytes < 1024) {
+      return bytes + ' Bytes';
+    }
+
+    if (bytes < 1048576) {
+      return (bytes / 1024).toFixed(2) + ' KB';
+    }
+
+    if (bytes < 1073741824) {
+      return (bytes / 1048576).toFixed(2) + ' MB';
+    }
+
+    return (bytes / 1073741824).toFixed(2) + ' GB';
   }
 
 }
